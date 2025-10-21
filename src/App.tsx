77511d179445483f05d587e578759c2f9d40040a
NikaNishnianidze <nikanishno8@gmail.com>
2025-10-21 14:13:27 +0400

    add responsive navigation design for tablet and desktop sizes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <div className="scroll-smooth">
+    <div className="scroll-smooth mb:p-6 tb:pt-8 tb:px-12 dk:pt-14 dk:px-28">
       <Navigation />
     </div>
   );
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,7 +7,7 @@ export default function Navigation() {
 
   return (
     <>
-      <div className="mobile-nav flex justify-between items-center p-6">
+      <div className="mb:block tb:hidden dk:hidden mobile-nav mb:flex mb:justify-between mb:items-center">
         <h1 className="text-[30px] text-white font-bold cursor-pointer">
           NicK
         </h1>
@@ -91,7 +91,80 @@ export default function Navigation() {
           </div>
         )}
       </div>
-      <div className="tablet+-nav"></div>
+      <div className="tablet+-nav mb:hidden tb:block dk:block tb:flex tb:flex-row tb:justify-between tb:items-center dk:flex dk:flex-row dk:justify-between dk:items-center">
+        <div className="logo">
+          <h1 className="text-[35px] text-white font-bold cursor-pointer">
+            NicK
+          </h1>
+        </div>
+        <div className="nav-lists">
+          <ul className="flex gap-[22px] justify-center text-white text-[16px] hover:text-gray">
+            <li>
+              <a
+                href="#home"
+                className={`hover:text-blue-500 ${
+                  chosen === "Home" ? "underline" : ""
+                }`}
+                onClick={() => setChosen("Home")}
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#about"
+                className={`hover:text-blue-500 ${
+                  chosen === "about" ? "underline" : ""
+                }`}
+                onClick={() => setChosen("about")}
+              >
+                About Me
+              </a>
+            </li>
+            <li>
+              <a
+                href="#contact"
+                className={`hover:text-blue-500 ${
+                  chosen === "services" ? "underline" : ""
+                }`}
+                onClick={() => setChosen("services")}
+              >
+                Services
+              </a>
+            </li>
+            <li>
+              <a
+                href="#contact"
+                className={`hover:text-blue-500 ${
+                  chosen === "portfolio" ? "underline" : ""
+                }`}
+                onClick={() => setChosen("portfolio")}
+              >
+                Portfolio
+              </a>
+            </li>
+            <li>
+              <a
+                href="#contact"
+                className={`hover:text-blue-500 ${
+                  chosen === "contact" ? "underline" : ""
+                }`}
+                onClick={() => setChosen("contact")}
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="connect-with-me flex">
+          <button
+            className="connect-button w-[162px] h-[50px] py-[15px] text-center text-[16px] rounded-[50px] cursor-pointer text-white bg-blue-600 
+               transition-transform duration-300 ease-in-out hover:scale-105 hover:bg-blue-700"
+          >
+            Connect With Me
+          </button>
+        </div>
+      </div>
     </>
   );
 }
